82caf87248a38ddb63b08da58bbfe0eeb6121687
abbas886 <abbas886@yahoo.com>
2017-10-03 16:20:16 +0530

    naming conventions

--- ServicesBackEnd/src/main/java/com/stackroute/activitystream/ServicesBackEnd/dao/UserDao.java
+++ ServicesBackEnd/src/main/java/com/stackroute/activitystream/ServicesBackEnd/dao/UserDao.java
@@ -7,8 +7,10 @@ import com.stackroute.activitystream.ServicesBackEnd.model.User;
 public interface UserDao {
 	public boolean registerUser(User user);
 	public List<User> getUsers();
+	//still you are not following naming conventions
 	public User getUserbyId(String mailid);
 	public boolean updateUser(User user);
 	public boolean deleteUser(String id);
+	//still you are not following naming conventions
 	public boolean validateUser(String usrid,String usrpass);
 }
