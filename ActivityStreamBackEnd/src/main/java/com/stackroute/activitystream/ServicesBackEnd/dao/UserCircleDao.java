171add5ab659a43c86524dea4b7c27fbded57d6d
abbas886 <abbas886@yahoo.com>
2017-10-05 15:34:49 +0530

    need more apis

--- ActivityStreamBackEnd/src/main/java/com/stackroute/activitystream/ServicesBackEnd/dao/UserCircleDao.java
+++ ActivityStreamBackEnd/src/main/java/com/stackroute/activitystream/ServicesBackEnd/dao/UserCircleDao.java
@@ -6,9 +6,15 @@ import com.stackroute.activitystream.ServicesBackEnd.model.UserCircle;
 
 public interface UserCircleDao {
 
+	//adUserToCircle
 	boolean addUserCircle(UserCircle usercircle);
+	//remveUserFromCircle
 	boolean removeUserCircle(String userCircleId);
+	//Why this method?
 	List<UserCircle> getAllUserCircle();
 	UserCircle getUserCircleById(String userCircleId );
 	public boolean updateUserCircle(UserCircle circle);
+	//you may need to get all the circle names / ids subscribed by a particular user
+	// like public String[] getMyCircles(String userID)  // if you want to get all circle names subscribed by a particular user
+	// like public int[] getMyCircles(String userID)  // if you want to get all circle ids subscribed by a particular user
 }
