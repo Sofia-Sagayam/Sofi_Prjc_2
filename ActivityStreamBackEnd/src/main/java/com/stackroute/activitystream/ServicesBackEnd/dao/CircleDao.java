083cd9973953de918d66233d1f7809c90e10207a
abbas886 <abbas886@yahoo.com>
2017-10-05 15:20:46 +0530

    comments added.

--- ActivityStreamBackEnd/src/main/java/com/stackroute/activitystream/ServicesBackEnd/dao/CircleDao.java
+++ ActivityStreamBackEnd/src/main/java/com/stackroute/activitystream/ServicesBackEnd/dao/CircleDao.java
@@ -3,11 +3,14 @@ package com.stackroute.activitystream.ServicesBackEnd.dao;
 import java.util.List;
 
 import com.stackroute.activitystream.ServicesBackEnd.model.Circle;
-
+//Why one method is public and others are default
 public interface CircleDao {
+	//what is c
 	boolean addCircle(Circle c);
 	boolean removeCircle(String circleId);
+	//the method name should be getAllCircles()
 	List<Circle> getAllCircle();
+	//You need one more method getCircleByName(String circleName);
 	Circle getCircleById(String circleId );
 	public boolean updateCircle(Circle circle);
 }
