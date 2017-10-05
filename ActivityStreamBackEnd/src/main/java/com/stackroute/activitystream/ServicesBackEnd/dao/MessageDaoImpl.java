d8562d6293ed48ce09ade943fbcc725e521bfcc2
abbas886 <abbas886@yahoo.com>
2017-10-05 15:31:36 +0530

    use private methods

--- ActivityStreamBackEnd/src/main/java/com/stackroute/activitystream/ServicesBackEnd/dao/MessageDaoImpl.java
+++ ActivityStreamBackEnd/src/main/java/com/stackroute/activitystream/ServicesBackEnd/dao/MessageDaoImpl.java
@@ -86,6 +86,7 @@ public class MessageDaoImpl implements MessageDao {
 			return false;}
 	}
 
+	//it should be private method
 @Override
 public boolean sendMessageToInbox(UserMessage message) {
 	try{
@@ -97,6 +98,7 @@ public boolean sendMessageToInbox(UserMessage message) {
 		return false;}
 }
 
+	//it should be private method
 @Override
 public boolean sendMessageToOutbox(Outbox message) {
 	try{
